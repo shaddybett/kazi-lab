952ce9a72e9656d7f0ae3d39be63e6e62ab80041
Shaddy <shadybett540@gmail.com>
2024-03-22 15:10:42 +0300

    created User model

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from flask_restful import Api
-from models import db
+from flask import Flask, jsonify
+from flask_restful import Api, Resource, request
+from models import db,User
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,3 +9,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jovial.db'
 app.config['TRACK_MODIFICATIONS'] = False
 db.init_app(api)
 
+
+class Signup(Resource):
+    def get(self):
+        data = request.get_json()
+        email = data.get('email')
+        existing        
+        
+
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        email = data.get('email')
+        password = data.get('password')
+        existing = User.query.filter_by(email=email).first()
--- server/models.py
+++ server/models.py
@@ -1,3 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer,primary_key=True)
+    first_name = db.Column(db.String(100),nullable=False)
+    last_name = db.Column(db.String(100),nullable=False)
+    email = db.Column(db.String(100),nullable=False)
+    password = db.Column(db.String,nullable=False)
+    
