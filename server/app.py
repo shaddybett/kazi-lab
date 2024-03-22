0078a94bb1e876576d365f6697e24e47ebd62591
Shaddy <shadybett540@gmail.com>
2024-03-22 23:38:36 +0300

    configured database

--- server/app.py
+++ server/app.py
@@ -70,6 +70,13 @@
 
 from flask import Flask
 from flask_restful import Api
+from models import db,User
 
 app = Flask(__name__)
 api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bett.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+
