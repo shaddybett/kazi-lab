107e243d4805d70d2d367953b5a24db61bd8fbe8
Shaddy <shadybett540@gmail.com>
2024-03-23 02:56:59 +0300

    initialized app

--- server/app.py
+++ server/app.py
@@ -10,7 +10,7 @@ bcrypt = Bcrypt(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jovial.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db.init_app(app)
+db.init_app(app)
 # password_pattern = re.compile(r'(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[$%@!*.!?])[A-Za-z\d$%@!*.!?]{8,}$')
 
 # signup_parser = reqparse.RequestParser()
