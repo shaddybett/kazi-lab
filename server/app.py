847dfe1ee3189cdc9f4206d3cc14bae9c0bdb4e8
Shaddy <shadybett540@gmail.com>
2024-03-23 11:05:45 +0300

    added more error handlers

--- server/app.py
+++ server/app.py
@@ -30,14 +30,16 @@ class Signup(Resource):
         args = signup_parser.parse_args()
         email = args['email']
         password = args['password']
+        if email == '' or password =='':
+            return jsonify({'error':'Fill in all forms'}),401
         existing_user = User.query.filter_by(email=email).first()
         if not password_pattern.match (password):
-            response = make_response({'error':'Password must meet the desired criteria'}, 400)
-            return response
+            return jsonify({'error':'invalid email or password'}),401
          
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
         if existing_user:
-            return jsonify({'error':'Email already exists'}),409
+            response = make_response({'error':'User already exists'},401)
+            return response
         else:
             newUser = User (
                 first_name = args['first_name'],
@@ -47,7 +49,8 @@ class Signup(Resource):
             )   
             db.session.add(newUser)
             db.session.commit()
-            return jsonify({'message':'User created successfully'}),201
+            response = make_response({'message':'User Created Successfully'},201)
+            return response
         
 login_parse = reqparse.RequestParser()
 login_parse.add_argument('email',type=str,required=True,help='email is required'),
@@ -57,19 +60,24 @@ class Login(Resource):
         args = login_parse.parse_args()
         email = args['email']
         password = args['password']
+        if email ==''or password =='':
+            response = make_response({'error':'Fill in all forms'},401)
+            return response
         existing_user = User.query.filter_by(email=email).first()
         if not existing_user:
-            return jsonify({'error':'Invalid email or password'}),401
+            response = make_response({'error':'Invalid email or password'},401)
         hashed_password = existing_user.password
         if bcrypt.check_password_hash(hashed_password,password):
-            return jsonify({'message':'Login successful'}),200
+            response = make_response({'message':'Login Successful'},200)
+            return response
         else:
-            return jsonify({'error':'Invalid email or password'}),401
+            response = make_response({'error':'Invalid email or password'},401)
+            return response
 api.add_resource(Signup,'/signup')
 api.add_resource(Login,'/login')
 
 if __name__=='__main__':
-    app.run(debug=True,port=5000)
+    app.run(debug=True,port=4000)
 
 
 
