0ffb801e41a9264b8fba91d6e0d82980b83f0496
Shaddy <shadybett540@gmail.com>
2024-03-23 23:59:48 +0300

    made handle submit variable

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,7 +2,13 @@ import React,{useState} from 'react'
 
 function Login() {
   const[email,setEmail] = useState('')
-  
+  const[password,setPassword] = useState('')
+  const[error,setError] = useState('')
+
+  const handleSubmit = () =>{
+    
+
+  }
 
   return (
     <div></div>
