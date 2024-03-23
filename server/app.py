c00a9dbbe6c06e799446f6daeea5d5510877750c
Shaddy <shadybett540@gmail.com>
2024-03-23 03:00:24 +0300

    made signup resource

--- server/app.py
+++ server/app.py
@@ -19,7 +19,7 @@ signup_parser.add_argument('last_name',type=str,required=True,help='Last name is
 signup_parser.add_argument('email',type=str,required=True,help='Email is required')
 signup_parser.add_argument('password',type=str,required=True,help='Password is required')
 
-# class Signup(Resource):
+class Signup(Resource)
 #     def post(self):
 #         args = signup_parser.parse_args()
 #         email = args['email']
