15327673068a0ee67fe6e8e2c4c7b97a007988ac
Shaddy <shadybett540@gmail.com>
2024-03-23 23:46:51 +0300

    done

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Link from 'react-router-dom'
 
 function Landing() {
   return (
-    <div>Landing</div>
+    <div>
+      <Link to='/login'>
+      <button>
+        login
+      </button>
+      </Link>
+      <p>Don't have an account? <Link to='/signup'>Signup</Link></p>
+    </div>
   )
 }
 
