fcc85184ddeb94b1964a3ad31d4a9b1274a8c6cd
Shaddy <shadybett540@gmail.com>
2024-03-23 23:29:05 +0300

    imported landing,login and signup into app.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Landing from './components/Landing'
+import Login from './components/Login'
+import Signup from './components/Signup'
+
+function App() {
+  return (
+    <div></div>
+  )
+}
+
+export default App
