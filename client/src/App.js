d9889265aa75deca5810cd4d482040cbda9e5e8b
Shaddy <shadybett540@gmail.com>
2024-03-23 23:37:22 +0300

    added routing to all files

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,15 @@ import React from 'react'
 import Landing from './components/Landing'
 import Login from './components/Login'
 import Signup from './components/Signup'
+import { Routes,Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div></div>
+    <Routes>
+      <Route path='/' element={<Landing/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+    </Routes>
   )
 }
 
