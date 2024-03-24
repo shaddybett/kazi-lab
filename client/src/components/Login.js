11c85091c5c45283872b09699ed67329489bad7b
Shaddy <shadybett540@gmail.com>
2024-03-24 14:44:44 +0300

    created new file

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,28 +1,43 @@
-import React,{useState} from 'react'
+import React,{useState} from "react";
+import {Link} from 'react-router-dom'
 
-function Login() {
-  const[email,setEmail] = useState('')
-  const[password,setPassword] = useState('')
-  const[error,setError] = useState('')
+const[email,setEmail] = useState('')
+const[password,setPassword] = useState('')
+const[error,setError] = useState('')
 
-  const handleLogin= (e)=>{
+const handleLogin = async(e)=>{
     e.preventDefault()
-  }
-  if (!email || !password) {
-    setError('All fields are required')
-    return;
-  }
-
-  try {
-    const response = await fetch('/login',)=>{}
-  }
-
-
-  
-
-  return (
-    <div></div>
-  )
 }
 
-export default Login
+try {
+    const handleLogin = await fetch('/login',{
+        method:'POST',
+        'Content-Type':'application/json',
+        headers: {
+            body:JSON.stringify({email,password})
+        }
+
+    })
+    const data = await response()
+    if (response.ok){
+        localStorage.setItem({'token':data.access_token}),
+        <Link to='/'/>
+    }
+    else{
+        setError('An error occurred')
+    }
+}
+catch{
+    setError('An error occurred please try again later')
+}
+
+return (
+    <div>
+        <form onSubmit={handleLogin}>
+
+        <input type="email" placeholder="example@gmail.com" value={email} onChange={setEmail}/>
+        <input type="password" placeholder="*********" value={password} onChange={setPassword}/>
+        <button type="submit">submit</button>
+        </form>
+    </div>
+)
--- server/app.py
+++ server/app.py
@@ -66,26 +66,27 @@ class Signup(Resource):
 login_parse = reqparse.RequestParser()
 login_parse.add_argument('email',type=str,required=True,help='email is required'),
 login_parse.add_argument('password',type=str,required=True,help='Password is required')
+
 class Login(Resource):
     def post(self):
         args = login_parse.parse_args()
         email = args['email']
         password = args['password']
-        if email ==''or password =='':
+        if email =='' or password=='':
             response = make_response({'error':'Fill in all forms'},401)
             return response
-        existing_user = User.query.filter_by(email=email).first()
-        if not existing_user:
-            response = make_response({'error':'Invalid email or password'},401)
+        existing = User.query.filter_by(email=email).first()
+        hashed_password = existing.password
+        if not existing:
+            response = make_response({'error':'Invalid Email or password'},401)
             return response
-        hashed_password = existing_user.password
         if bcrypt.check_password_hash(hashed_password,password):
-            access_token = create_access_token(identity=email)
-            response = make_response({'message':'Login Successful', 'access_token':access_token},200)
+            response = make_response({'message':'Login successful'},200)
             return response
-        else:
-            response = make_response({'error':'Invalid email or password'},401)
-            return response
+        response = make_response({'error':'Invalid email or password'},401)
+        return response
+        
+        
 api.add_resource(Signup,'/signup')
 api.add_resource(Login,'/login')
 
