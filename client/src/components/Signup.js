53e62488ba5aeade3226a440c77f871031f2be84
Shaddy <shadybett540@gmail.com>
2024-03-25 13:43:36 +0300

    set up signup.js

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,19 +1,48 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {Link,useNavigate} from 'react-router-dom'
 
 function Signup() {
-  const formData = 
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+  const [firstName,setFirstName] = useState('')
+  const [lastName,setLastName] = useState('')
+  const [error,setError] = useState('')
+  const navigate = useNavigate()
   const handleSignup = async(e)=>{
     e.preventDefault()
-    const response = await fetch ('/signup',{
-      method:'POST',
-      headers:{
-        'Content-Type':'application/json'
-      },
-      body:JSON.stringify({formData})
-    })
+    try {
+      const response = await fetch ('/signup',{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify({firstName,lastName,email,password})
+      })
+      if (response.ok){
+        navigate('/login')
+      }
+      else {
+        const errorMessage = await response.json()
+        setError(errorMessage.error)
+      }
+    }
+    catch{
+      setError('An error occurred please try again later!')
+    }
+
   }
   return (
-    <div>Signup</div>
+    <div>
+      <form onSubmit={handleSignup}>
+        <input type='text' placeholder='Enter your first name' value={firstName} onChange={(e)=>setFirstName(e.target.value)}/>
+        <input type='text' placeholder='Enter your last name' value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
+        <input type='email' placeholder='Enter your email'/>
+        <input type='password' placeholder='Enter your password'/>
+        <button type='submit'>Submit</button>
+        {error && <p>{error}</p>}
+      </form>
+      <p>Have an account? <Link to='/login'>Login</Link></p>
+    </div>
   )
 }
 
