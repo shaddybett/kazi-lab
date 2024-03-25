b1a0472714356f204dfeb436dc18a143e34167c9
Shaddy <shadybett540@gmail.com>
2024-03-25 21:30:05 +0300

    fixed errors

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Login from './components/Login'
 import Signup from './components/Signup'
 import { Routes,Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
-import Home from './components/Home'
+
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Route path='/login' element={<Login/>}/>
       <Route path='/signup' element={<Signup/>}/>
       <Route path='/dashboard' element={<Dashboard/>}/>
-      <Route path='/home' element={<Home/>}/>
+     
     </Routes>
   )
 }
