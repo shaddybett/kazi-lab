698b23fa2cfab09dad4151184b83a0306a18926b
Shaddy <shadybett540@gmail.com>
2024-03-26 21:51:33 +0300

    done

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/lib/**/*.js",
     "./pages/**/*.{ts,tsx}",
     "./public/**/*.html",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 }
 
