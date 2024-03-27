e86336b904756975601c9d3d12900f1cfc6e2211
Shaddy <shadybett540@gmail.com>
2024-03-27 22:24:47 +0300

    changed navigation after provider's login

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Dashboard from './components/Dashboard'
 import ClientDashboard from './components/ClientDashboard'
 import ProviderDashboard from './components/ProviderDashboard'
 import Profile from './components/Profile'
+import ProviderDetails from './components/ProviderDetails'
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
       <Route path='/clientPage' element={<ClientDashboard/>}/>
       <Route path='/providerPage' element={<ProviderDashboard/>}/>
       <Route path='/profile' element={<Profile/>}/>
+      <Route path='/provider-details' element={<ProviderDetails/>}/>
      
     </Routes>
   )
---
+++ client/src/components/ProviderDetails.js
@@ -0,0 +1,43 @@
+import React,{useEffect, useState} from 'react'
+
+
+function ProviderDetails() {
+    const [data,setData] = useState([])
+    const [error,setError] = useState('')
+
+useEffect(()=>{
+    const handleEntry = async()=>{
+        try{
+            const token = localStorage.getItem('token')
+            const response = await fetch('/dashboard',{
+                method:'GET',
+                headers:{
+                    'Content-Type':'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            if (response.ok){
+                const responseData = await response.json()
+                setData(responseData)
+            }else {
+                const errorMessage = await response.json()
+                setError(errorMessage)
+            }
+        }
+        catch(error){
+            setError('An error occurred. Please try again later')
+        }
+    }
+    handleEntry()
+},[])
+    
+    
+
+  return (
+    <div>
+        <h3>Hello {data.first_name} Complete your signup by filling in the form below</h3>
+    </div>
+  )
+}
+
+export default ProviderDetails
