59d336f251dff927131c50a8d84cfbd89068a1a9
Shaddy <shadybett540@gmail.com>
2024-03-27 12:54:49 +0300

    done

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,6 +5,7 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [data, setData] = useState("");
 
   const navigate = useNavigate();
 
@@ -21,14 +22,6 @@ function Login() {
       if (response.ok) {
         const data = await response.json();
         localStorage.setItem("token", data.access_token);
-
-        if (data.role_id === 2) {
-          console.log(data.role_id);
-          navigate("/providerPage");
-        } else {
-          console.log("Redirecting to providerPage");
-          navigate("/clientPage");
-        }
       } else {
         const errorMessage = await response.json();
         setError(errorMessage.error);
@@ -36,7 +29,40 @@ function Login() {
     } catch (error) {
       setError("An error occurred.Please try again later");
     }
+
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        throw new Error("Token not found");
+      }
+      const response = await fetch("/dashboard", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log(responseData);
+        setData(responseData);
+      } else {
+        const errorMessage = await response.json();
+        console.log(errorMessage);
+        setError(errorMessage.error || "An error occurred");
+      }
+    } catch (error) {
+      setError("An error occurred. Please try again later.");
+    }
   };
+  if (data.role_id === 2) {
+    console.log(data.role_id);
+    navigate("/clientPage");
+  } else {
+    console.log("Redirecting to providerPage");
+    navigate("/providerPage");
+  }
+
   return (
     <div>
       <form onSubmit={handleLogin}>
