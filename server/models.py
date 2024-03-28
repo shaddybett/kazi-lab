b905e3041ddccb199b8bd9365878e539b09abef2
Shaddy <shadybett540@gmail.com>
2024-03-29 02:01:46 +0300

    removed migration instances

--- server/models.py
+++ server/models.py
@@ -1,37 +1,73 @@
+# from flask_sqlalchemy import SQLAlchemy
+
+# db = SQLAlchemy()
+
+# class User(db.Model):
+#     __tablename__='users'
+#     id = db.Column(db.Integer,primary_key=True, autoincrement=True)
+#     first_name = db.Column(db.String(100),nullable=False)
+#     middle_name = db.Column(db.String(100),nullable=True)
+#     last_name = db.Column(db.String(100),nullable=False)
+#     national_id = db.Column(db.Integer,nullable=True,unique=True)
+#     phone_number = db.Column(db.Integer,nullable=True,unique=True)
+#     email = db.Column(db.String(100),nullable=False,unique=True)
+#     password = db.Column(db.String(255),nullable=False)
+#     image = db.Column(db.String,nullable=True)
+#     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'),nullable=False,)
+#     role = db.relationship('Role', backref = db.backref('users',lazy=True))
+#     services = db.relationship('Service', backref=db.backref('provider', lazy=True))
+
+
+# class Service(db.Model):
+#     __tablename__='services'
+#     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+#     service_name = db.Column(db.String,nullable=False)
+#     providers = db.relationship('User', secondary='provider_services', backref=db.backref('services', lazy=True))
+#     provider_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+# class ProviderService(db.Model):
+#     __tablename__='provider_services'
+#     service_id = db.Column(db.Integer,db.ForeignKey('services.id'),primary_key=True)
+#     provider_id = db.Column(db.Integer,db.ForeignKey('users.id',primary_key=True))    
+
+
+# class Role(db.Model):
+#     __tablename__='roles'    
+#     id = db.Column(db.Integer,primary_key=True)
+#     role_name=db.Column(db.String,nullable=False)
+
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__='users'
-    id = db.Column(db.Integer,primary_key=True, autoincrement=True)
-    first_name = db.Column(db.String(100),nullable=False)
-    middle_name = db.Column(db.String(100),nullable=True)
-    last_name = db.Column(db.String(100),nullable=False)
-    national_id = db.Column(db.Integer,nullable=True,unique=True)
-    phone_number = db.Column(db.Integer,nullable=True,unique=True)
-    email = db.Column(db.String(100),nullable=False,unique=True)
-    password = db.Column(db.String(255),nullable=False)
-    image = db.Column(db.String,nullable=True)
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'),nullable=False,)
-    role = db.relationship('Role', backref = db.backref('users',lazy=True))
-    services = db.relationship('Service', backref=db.backref('provider', lazy=True))
-
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    middle_name = db.Column(db.String(100), nullable=True)
+    last_name = db.Column(db.String(100), nullable=False)
+    national_id = db.Column(db.Integer, nullable=True, unique=True)
+    phone_number = db.Column(db.Integer, nullable=True, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String, nullable=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
+    role = db.relationship('Role', backref=db.backref('users', lazy=True))
+    services = db.relationship('Service', secondary='provider_services', backref=db.backref('providers', lazy=True))
 
 class Service(db.Model):
-    __tablename__='services'
-    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
-    service_name = db.Column(db.String,nullable=False)
-    providers = db.relationship('User', secondary='provider_services', backref=db.backref('services', lazy=True))
+    __tablename__ = 'services'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    service_name = db.Column(db.String(100), nullable=False)
     provider_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 class ProviderService(db.Model):
-    __tablename__='provider_services'
-    service_id = db.Column(db.Integer,db.ForeignKey('services.id'),primary_key=True)
-    provider_id = db.Column(db.Integer,db.ForeignKey('users.id',primary_key=True))    
-
+    __tablename__ = 'provider_services'
+    service_id = db.Column(db.Integer, db.ForeignKey('services.id'), primary_key=True)
+    provider_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)    
 
 class Role(db.Model):
-    __tablename__='roles'    
-    id = db.Column(db.Integer,primary_key=True)
-    role_name=db.Column(db.String,nullable=False)
+    __tablename__ = 'roles'    
+    id = db.Column(db.Integer, primary_key=True)
+    role_name = db.Column(db.String(100), nullable=False)
