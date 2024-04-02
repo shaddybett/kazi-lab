2e5fa6e48b1b6d06c8e293a685a494833dc71f2d
Shaddy <shadybett540@gmail.com>
2024-04-02 20:56:45 +0300

    added handle services line

--- client/src/components/ClientDashboard.js
+++ client/src/components/ClientDashboard.js
@@ -64,6 +64,7 @@ function ClientDashboard() {
         setError('An error occurred. Try again later')
       }
     }
+    handleServices();
     handleEntry();
   }, []);
   return (
@@ -105,12 +106,8 @@ function ClientDashboard() {
       </Navbar>
       <Card className="max-w-sm">
         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Noteworthy technology acquisitions 2021
+          {services && <p>{services.service_name}</p>}
         </h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </p>
         <Button>
           Service Providers
           <svg
