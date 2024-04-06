c0403b724a2660aea9e71b2b8754f66daf149206
Shaddy <shadybett540@gmail.com>
2024-04-07 00:23:44 +0300

    fexed errors in resource fetching user details

--- server/app.py
+++ server/app.py
@@ -224,27 +224,25 @@ class ServiceProvider(Resource):
             return response
 
 
-# provider_list = reqparse.RequestParser()
-# provider_list.add_argument('provider_ids', type=int, required=True, help='Provider ID is required')
-
 class ProviderList(Resource):
     @jwt_required()
-    def get(self,provider_ids):
-        # args = provider_list.parse_args()
-        # provider_ids = args['provider_ids']
-
+    def get(self):
+        provider_ids = request.args.getlist('provider_ids')
+        if not provider_ids:
+            return {'error': 'No provider IDs provided in the request'}, 400
+        
         # Query User table to get user details based on provider IDs
         users = User.query.filter(User.id.in_(provider_ids)).all()
 
         if users:
             # Extract first names of users
-            
             first_names = [user.first_name for user in users]
             response = make_response({'first_names': first_names})
             return response
         else:
             # No users found for the given provider IDs
             return {'error': 'No users found for the given provider IDs'}, 404
+
 
 class ProviderIds(Resource):
     def get(self, service_id):
