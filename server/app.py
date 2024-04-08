b3473370f31c9c7994185f6f9910605d5e57645d
Shaddy <shadybett540@gmail.com>
2024-04-09 00:00:46 +0300

    modified provider resource  to accept a list

--- server/app.py
+++ server/app.py
@@ -224,29 +224,13 @@ class ServiceProvider(Resource):
             return response
 
 
-class ProviderList(Resource):
-    # @jwt_required()
-    def get(self, provider_ids):
-        if isinstance(provider_ids, int):
-            provider_ids = [provider_ids]
-        else:
-            provider_ids = provider_ids.split(',')
-        # Query User table to get user details based on provider IDs
-        users = User.query.filter(User.id.in_(provider_ids)).all()
-
-        if users:
-            # Extract first names of users
-            first_names = [user.first_name for user in users]
-            response = make_response({'first_names': first_names})
-            return response
-        else:
-            # No users found for the given provider IDs
-            return {'error': 'No users found for the given provider IDs'}, 404
-
 # class ProviderList(Resource):
 #     # @jwt_required()
-#     def get(self,provider_ids):
-#         provider_ids = request.args.getlist('provider_ids')
+#     def get(self, provider_ids):
+#         if isinstance(provider_ids, int):
+#             provider_ids = [provider_ids]
+#         else:
+#             provider_ids = provider_ids.split(',')
 #         # Query User table to get user details based on provider IDs
 #         users = User.query.filter(User.id.in_(provider_ids)).all()
 
@@ -259,7 +243,26 @@ class ProviderList(Resource):
 #             # No users found for the given provider IDs
 #             return {'error': 'No users found for the given provider IDs'}, 404
 
+class ProviderList(Resource):
+    # @jwt_required()
+    def get(self, provider_ids):
+        # Split the comma-separated string into a list of IDs
+        provider_ids_list = provider_ids.split(',')
 
+        # Convert each ID to integer
+        provider_ids_list = [int(provider_id) for provider_id in provider_ids_list]
+
+        # Query User table to get user details based on provider IDs
+        users = User.query.filter(User.id.in_(provider_ids_list)).all()
+
+        if users:
+            # Extract first names of users
+            first_names = [user.first_name for user in users]
+            response = make_response({'first_names': first_names})
+            return response
+        else:
+            # No users found for the given provider IDs
+            return {'error': 'No users found for the given provider IDs'}, 404
 
 
 class ProviderIds(Resource):
