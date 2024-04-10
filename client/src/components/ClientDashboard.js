503cc4d3f22737a0aedc08fec649e38e9a1bf4cf
Shaddy <shadybett540@gmail.com>
2024-04-10 15:19:36 +0300

    done

--- client/src/components/ClientDashboard.js
+++ client/src/components/ClientDashboard.js
@@ -1,171 +1,171 @@
-// import React, { useState, useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { Avatar, Dropdown, Navbar, Button, Card } from "flowbite-react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Avatar, Dropdown, Navbar, Button, Card } from "flowbite-react";
 
-// function ClientDashboard() {
-//   const [data, setData] = useState({});
-//   const [error, setError] = useState("");
-//   const [services, setServices] = useState([]);
-//   const [providers, setProviders] = useState([]);
-//   const [providerIds, setProviderIds] = useState([]);
-//   const navigate = useNavigate();
+function ClientDashboard() {
+  const [data, setData] = useState({});
+  const [error, setError] = useState("");
+  const [services, setServices] = useState([]);
+  const [providers, setProviders] = useState([]);
+  const [providerIds, setProviderIds] = useState([]);
+  const navigate = useNavigate();
 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const token = localStorage.getItem("token");
-//         const response = await fetch("/service", {
-//           method: "GET",
-//           headers: {
-//             "Content-Type": "application/json",
-//             Authorization: `Bearer ${token}`,
-//           },
-//         });
-//         if (response.ok) {
-//           const responseData = await response.json();
-//           setServices(responseData.all_services);
-//         } else {
-//           const errorMessage = await response.json();
-//           setError(errorMessage.error || "An error occurred");
-//         }
-//       } catch (error) {
-//         setError("An error occurred. Try again later");
-//       }
-//     };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await fetch("/service", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (response.ok) {
+          const responseData = await response.json();
+          setServices(responseData.all_services);
+        } else {
+          const errorMessage = await response.json();
+          setError(errorMessage.error || "An error occurred");
+        }
+      } catch (error) {
+        setError("An error occurred. Try again later");
+      }
+    };
 
-//     const handleUser = async () => {
-//       try {
-//         const token = localStorage.getItem("token");
-//         const response = await fetch("/dashboard", {
-//           method: "GET",
-//           headers: {
-//             "Content-Type": "application/json",
-//             Authorization: `Bearer ${token}`,
-//           },
-//         });
-//         if (response.ok) {
-//           const responseData = await response.json();
-//           setData(responseData);
-//         } else {
-//           const errorMessage = await response.json();
-//           setError(errorMessage.error || "An error occurred");
-//         }
-//       } catch (error) {
-//         setError("An error occurred. Please try again later");
-//       }
-//     };
+    const handleUser = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await fetch("/dashboard", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (response.ok) {
+          const responseData = await response.json();
+          setData(responseData);
+        } else {
+          const errorMessage = await response.json();
+          setError(errorMessage.error || "An error occurred");
+        }
+      } catch (error) {
+        setError("An error occurred. Please try again later");
+      }
+    };
 
-//     fetchData();
-//     handleUser();
-//   }, []);
+    fetchData();
+    handleUser();
+  }, []);
 
-//   const handleProviders = async (service) => {
-//     try {
-//       const token = localStorage.getItem("token");
-//       const response = await fetch(`/provider-ids/${service.id}`, {
-//         method: "GET",
-//         headers: {
-//           "Content-Type": "application/json",
-//           Authorization: `Bearer ${token}`,
-//         },
-//       });
-//       if (response.ok) {
-//         const responseData = await response.json();
-//         localStorage.setItem("providerIds", JSON.stringify(responseData.provider_ids));
-//         const providerIds = responseData.provider_ids.join(",");
-//         const userResponse = await fetch(`/provider-details?provider_ids=${providerIds}`, {
-//           method: "GET",
-//           headers: {
-//             "Content-Type": "application/json",
-//             Authorization: `Bearer ${token}`,
-//           },
-//         });
-//         if (userResponse.ok) {
-//           const userData = await userResponse.json();
+  const handleProviders = async (service) => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await fetch(`/provider-ids/${service.id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        const responseData = await response.json();
+        localStorage.setItem("providerIds", JSON.stringify(responseData.provider_ids));
+        const providerIds = responseData.provider_ids.join(",");
+        const userResponse = await fetch(`/provider-details?provider_ids=${providerIds}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (userResponse.ok) {
+          const userData = await userResponse.json();
 
-//           const providerNames = userData.first_names;
-//           setProviders(providerNames);
-//           navigate("/providers");
-//         } else {
-//           const errorMessage = await userResponse.json();
-//           setError(errorMessage.error);
-//         }
-//       } else {
-//         const errorMessage = await response.json();
-//         setError(
-//           errorMessage.error || "An error occurred while fetching provider IDs"
-//         );
-//       }
-//     } catch (error) {
-//       setError("An error occurred please try again later");
-//     }
-//   };
+          const providerNames = userData.first_names;
+          setProviders(providerNames);
+          navigate("/providers");
+        } else {
+          const errorMessage = await userResponse.json();
+          setError(errorMessage.error);
+        }
+      } else {
+        const errorMessage = await response.json();
+        setError(
+          errorMessage.error || "An error occurred while fetching provider IDs"
+        );
+      }
+    } catch (error) {
+      setError("An error occurred please try again later");
+    }
+  };
 
-//   const handleLogout = () => {
-//     localStorage.removeItem("token");
-//     navigate("/login");
-//   };
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
 
-//   const handleProfile = () => {
-//     navigate("/profile");
-//   };
+  const handleProfile = () => {
+    navigate("/profile");
+  };
 
-//   return (
-//     <div>
-//       <Navbar fluid rounded className="bg-black">
-//         <div className="flex md:order-2">
-//           <Dropdown
-//             arrowIcon={false}
-//             inline
-//             label={
-//               <Avatar
-//                 alt="User settings"
-//                 img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-//                 rounded
-//               />
-//             }
-//           >
-//             <Dropdown.Header>
-//               <span className="block text-sm">
-//                 {data.first_name} {data.last_name}
-//               </span>
-//               <span className="block truncate text-sm font-medium">
-//                 {data.email}
-//               </span>
-//             </Dropdown.Header>
-//             <Dropdown.Item onClick={handleProfile}>Profile</Dropdown.Item>
-//             <Dropdown.Divider />
-//             <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
-//           </Dropdown>
-//           <Navbar.Toggle />
-//         </div>
-//         <Navbar.Collapse>
-//           <Navbar.Link href="/link1" active>
+  return (
+    <div>
+      <Navbar fluid rounded className="bg-black">
+        <div className="flex md:order-2">
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                alt="User settings"
+                img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                rounded
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">
+                {data.first_name} {data.last_name}
+              </span>
+              <span className="block truncate text-sm font-medium">
+                {data.email}
+              </span>
+            </Dropdown.Header>
+            <Dropdown.Item onClick={handleProfile}>Profile</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
+          </Dropdown>
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse>
+          <Navbar.Link href="/link1" active>
 
-//           </Navbar.Link>
-//           <Navbar.Link href="/link2"></Navbar.Link>
-//           <Navbar.Link href="/link3"></Navbar.Link>
-//           <Navbar.Link href="/link4"></Navbar.Link>
-//           <Navbar.Link href="/link5"></Navbar.Link>
-//         </Navbar.Collapse>
-//       </Navbar>
-//       <Card className="max-w-sm">
-//         {services.map((service) => (
-//           <div key={service.id} className="mb-4">
-//             <Card className="max-w-sm">
-//               <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-//                 {service.name}
-//               </h5>
-//               <Button onClick={() => handleProviders(service)}>
-//                 Service Providers
-//               </Button>
-//             </Card>
-//           </div>
-//         ))}
-//       </Card>
-//       {error && <p>{error}</p>}
-//     </div>
-//   );
-// }
+          </Navbar.Link>
+          <Navbar.Link href="/link2"></Navbar.Link>
+          <Navbar.Link href="/link3"></Navbar.Link>
+          <Navbar.Link href="/link4"></Navbar.Link>
+          <Navbar.Link href="/link5"></Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+      <Card className="max-w-sm">
+        {services.map((service) => (
+          <div key={service.id} className="mb-4">
+            <Card className="max-w-sm">
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {service.name}
+              </h5>
+              <Button onClick={() => handleProviders(service)}>
+                Service Providers
+              </Button>
+            </Card>
+          </div>
+        ))}
+      </Card>
+      {error && <p>{error}</p>}
+    </div>
+  );
+}
 
-// export default ClientDashboard;
+export default ClientDashboard;
