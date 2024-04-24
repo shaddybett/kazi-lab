e167f6a7d2308dbf29263cc6eec50c34de9d2942
Shaddy <shadybett540@gmail.com>
2024-04-24 21:17:51 +0300

    removed the fetched user data

--- client/src/components/ProviderDetails.js
+++ client/src/components/ProviderDetails.js
@@ -202,16 +202,8 @@ function ProviderDetails() {
       }
     };
     fetchData();
-    const userData = JSON.parse(localStorage.getItem("userData"));
-    if (userData) {
-      setFirst_name(userData.first_name || "");
-      setLast_name(userData.last_name || "");
-      setEmail(userData.email || "");
-      setPassword(userData.password || "");
-      setSelectedRole(userData.role_id || "");
-    }
-  }, []);
 
+  }, []);
   const handleCheckboxChange = (service) => {
     const selectedIndex = selectedServices.findIndex(
       (s) => s.id === service.id
