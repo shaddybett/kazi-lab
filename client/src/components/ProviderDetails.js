b1231d2477afacfee05d2cec6c66dc3e819d1518
Shaddy <shadybett540@gmail.com>
2024-04-24 09:51:01 +0300

    stringified phone number

--- client/src/components/ProviderDetails.js
+++ client/src/components/ProviderDetails.js
@@ -165,7 +165,7 @@ function ProviderDetails() {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify({middle_name,national_id}),
+        body: JSON.stringify({middle_name,national_id,phone_number}),
       });
 
       // Check if both requests were successful
