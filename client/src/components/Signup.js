5d2da88c36e986ca334b9a5bb032df25399b5889
Shaddy <shadybett540@gmail.com>
2024-04-29 17:38:07 +0300

    removed variable fetching role id from the local strage

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -53,7 +53,6 @@ function Signup() {
         const responseData = await response.json();
         localStorage.setItem("token", responseData.token);
         localStorage.setItem("id", responseData.id);
-        
         console.log(responseData.role_id);
         if (responseData.role_id === 2) {
           console.log("here we go");
