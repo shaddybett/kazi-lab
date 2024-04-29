3b44df0f021755076d66c538acd3818baa37f5ff
Shaddy <shadybett540@gmail.com>
2024-04-29 19:14:29 +0300

    updated phone number

--- server/app.py
+++ server/app.py
@@ -380,6 +380,7 @@ class Signup(Resource):
             return {'error':'User not found'},404
         user.middle_name = middle_name
         user.national_id = national_id
+        user.phone_number = phone_number
         new_user = User(
             first_name=first_name,
             last_name=last_name,
