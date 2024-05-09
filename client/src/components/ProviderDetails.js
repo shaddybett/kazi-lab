cff529f7773f1c63b4f0d4480092470eb1b67b0c
Shaddy <shadybett540@gmail.com>
2024-05-09 11:33:43 +0300

    made a form data variable

--- client/src/components/ProviderDetails.js
+++ client/src/components/ProviderDetails.js
@@ -1,3 +1,172 @@
+// import React, { useEffect, useState } from "react";
+// import { Dropdown,FileInput, Label } from "flowbite-react";
+// import { useNavigate } from "react-router-dom";
+
+// function ProviderDetails() {
+//   const [data, setData] = useState([]);
+//   const [selectedServices, setSelectedServices] = useState([]);
+//   const [newServiceName, setNewServiceName] = useState("");
+//   const [error, setError] = useState("");
+//   const [middle_name, setMiddle] = useState("");
+//   const [phone_number, setNumber] = useState("");
+//   const [national_id, setN_id] = useState("");
+//   const [message, setMessage] = useState("");
+//   const [image,setImage] = useState(null);
+//   const navigate = useNavigate();
+
+//   const handleServiceFormSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const token = localStorage.getItem("token");
+//       const id = localStorage.getItem("id");
+//       const uuid = localStorage.getItem("signupUUID");
+//       console.log(uuid);
+
+//       // Send request to add services
+//       const serviceRequestBody = {
+//         user_id: id,
+//         existing_services: selectedServices.map((service) => service.id),
+//         service_name: newServiceName.trim() !== "" ? newServiceName : null,
+//       };
+//       const serviceResponse = await fetch("/service", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//           Authorization: `Bearer ${token}`,
+//         },
+//         body: JSON.stringify(serviceRequestBody),
+//       });
+//       console.log(middle_name,national_id,phone_number,uuid)
+//       const userDetailsResponse = await fetch("/signup2", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//           middle_name,
+//           national_id,
+//           phone_number,
+//           uids:localStorage.getItem('signupUUID'),
+//           image
+//         }),
+
+
+//       });
+
+//       // Check if both requests were successful 
+//       if (serviceResponse.ok && userDetailsResponse.ok) {
+//         const serviceData = await serviceResponse.json();
+//         const userDetailsData = await userDetailsResponse.json();
+//         setMessage("Services and user details added successfully");
+//         navigate("/providerPage");
+//       } else {
+//         // Handle errors if any request fails
+//         const serviceErrors = await serviceResponse.json();
+//         const userDetailsErrors = await userDetailsResponse.json();
+//         setError(serviceErrors.error || userDetailsErrors.error);
+//       }
+//     } catch (error) {
+//       setError(error.message || "An error occurred. Please try again later.");
+//     }
+//   };
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const token = localStorage.getItem("token");
+//         const response = await fetch("/service", {
+//           method: "GET",
+//           headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${token}`,
+//           },
+//         });
+//         if (response.ok) {
+//           const responseData = await response.json();
+//           setData(responseData.all_services);
+//         } else {
+//           const errorMessage = await response.json();
+//           setError(errorMessage.error);
+//         }
+//       } catch (error) {
+//         setError("An error occurred. Please try again later.");
+//       }
+//     };
+//     fetchData();
+//   }, []);
+//   const handleCheckboxChange = (service) => {
+//     const selectedIndex = selectedServices.findIndex(
+//       (s) => s.id === service.id
+//     );
+//     if (selectedIndex === -1) {
+//       setSelectedServices([...selectedServices, service]);
+//     } else {
+//       setSelectedServices(selectedServices.filter((s) => s.id !== service.id));
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={handleServiceFormSubmit}>
+//         <Dropdown label="Services">
+//           {data &&
+//             data.map((service) => (
+//               <Dropdown.Item key={service.id} className="text-black">
+//                 <label>
+//                   <input
+//                     type="checkbox"
+//                     value={service.id}
+//                     onChange={() => handleCheckboxChange(service)}
+//                     checked={selectedServices.some((s) => s.id === service.id)}
+//                   />
+//                   {service.name}
+//                 </label>
+//               </Dropdown.Item>
+//             ))}
+//         </Dropdown>
+
+//         <input
+//           type="text"
+//           placeholder="mama Junior"
+//           value={middle_name}
+//           onChange={(e) => setMiddle(e.target.value)}
+//         />
+//         <input
+//           type="text"
+//           placeholder="0722000000"
+//           value={phone_number}
+//           onChange={(e) => setNumber(e.target.value)}
+//         />
+//         <input
+//           type="text"
+//           placeholder="12345678"
+//           value={national_id}
+//           onChange={(e) => setN_id(e.target.value)}
+//         />
+//         <input
+//           type="text"
+//           value={newServiceName}
+//           onChange={(e) => setNewServiceName(e.target.value)}
+//           placeholder="Enter new service name"
+//         />
+
+//         <div className="mb-2 block">
+//           <Label htmlFor="file-upload" value={image} onChange={(e)=> setImage(e.target.value)} />
+//         </div>
+//         <FileInput id="file-upload" />
+
+//         <button type="submit">Submit</button>
+//       </form>
+
+//       {error && <p>{error}</p>}
+//       {message && <p>{message}</p>}
+//     </div>
+//   );
+// }
+
+// export default ProviderDetails;
+
+
 import React, { useEffect, useState } from "react";
 import { Dropdown,FileInput, Label } from "flowbite-react";
 import { useNavigate } from "react-router-dom";
@@ -11,7 +180,7 @@ function ProviderDetails() {
   const [phone_number, setNumber] = useState("");
   const [national_id, setN_id] = useState("");
   const [message, setMessage] = useState("");
-  const [image,setImage] = useState(null)
+  const [image,setImage] = useState(null);
   const navigate = useNavigate();
 
   const handleServiceFormSubmit = async (e) => {
@@ -36,6 +205,8 @@ function ProviderDetails() {
         },
         body: JSON.stringify(serviceRequestBody),
       });
+      const formData = new FormData();
+      
       console.log(middle_name,national_id,phone_number,uuid)
       const userDetailsResponse = await fetch("/signup2", {
         method: "POST",
