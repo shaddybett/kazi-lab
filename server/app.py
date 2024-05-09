90d97794c9dac78764529aca715fa613528ceed3
Shaddy <shadybett540@gmail.com>
2024-05-09 13:43:18 +0300

    done

--- server/app.py
+++ server/app.py
@@ -98,7 +98,6 @@ class Signup(Resource):
 
 UPLOAD_FOLDER = 'server/userImages'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif' }
-
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 class signup2(Resource):
