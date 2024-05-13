9c8d7277f73594eed6b3e56b4a14b2f66d516833
Shaddy <shadybett540@gmail.com>
2024-05-13 23:13:12 +0300

    removed get keyword

--- server/app.py
+++ server/app.py
@@ -118,10 +118,10 @@ class signup2(Resource):
             for key, file in request.files.items():
                 print(f"File Field - {key}: {file.filename}")              
             middle_name = request.form['middle_name']
-            national_id = request.form.get['national_id']
-            phone_number = request.form.get['phone_number']
-            uids = request.form.get['uids']
-            image_file = request.files.get['image']
+            national_id = request.form['national_id']
+            phone_number = request.form['phone_number']
+            uids = request.form['uids']
+            image_file = request.files['image']
             print("Middle Name:", middle_name)
             print("National ID:", national_id)
             print("Phone Number:", phone_number)
