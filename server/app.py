163d8573ce8d9f91268cef0562d54cdba1d5e7aa
Shaddy <shadybett540@gmail.com>
2024-05-21 21:13:06 +0300

    added national id to response

--- server/app.py
+++ server/app.py
@@ -149,7 +149,7 @@ class signup2(Resource):
                 existing_user.uids = uids
 
                 db.session.commit()
-                return {'message':'user details updated successfully','middle_name':middle_name,}
+                return {'message':'user details updated successfully','middle_name':middle_name,'national_id':national_id,}
             else:
                 return {'error':'Update failed'}
         except Exception as e:
