a527014264f4f28ece17eb241465afcc4fd49aae
Shaddy <shadybett540@gmail.com>
2024-05-23 18:44:58 +0300

    changed tag to ul

--- client/src/components/ProviderDashboard.js
+++ client/src/components/ProviderDashboard.js
@@ -106,7 +106,7 @@ function ProviderDashboard() {
       <div>
         <h2>Hello, {data.first_name} welcome </h2>
         <h1>Services you offer</h1>
-        {service && <ol><li>{service}</li></ol>}
+        {service && <ul><li>{service}</li></ul>}
       </div>
       {error && <p>{error}</p>}
     </div>
