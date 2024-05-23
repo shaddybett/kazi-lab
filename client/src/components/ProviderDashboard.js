f2c8911b3a8040afd8bcdf364b257f87e973cdd6
Shaddy <shadybett540@gmail.com>
2024-05-23 12:43:20 +0300

    printed token in the console

--- client/src/components/ProviderDashboard.js
+++ client/src/components/ProviderDashboard.js
@@ -19,6 +19,7 @@ function ProviderDashboard() {
   useEffect(() => {
     const handleEntry = async () => {
       const token = localStorage.getItem("token");
+      console.log(token)
       try {
         const response = await fetch("/dashboard", {
           method: "GET",
