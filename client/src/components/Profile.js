00fe2a87eef64b390a1c7a258ac885641db25df5
Shaddy <shadybett540@gmail.com>
2024-05-24 13:01:40 +0300

    added confirm buttons and colors

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -73,6 +73,9 @@ function Profile() {
         text: "You won't be able to revert this!",
         icon: "warning",
         showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
     })
     try {
       const token = localStorage.getItem('token');
