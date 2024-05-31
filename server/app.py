d9e0dc0608a780048bc38c742e24f8101db90a8c
Shaddy <shadybett540@gmail.com>
2024-05-31 21:11:32 +0300

    made a query to provider service model to fetch service ids

--- server/app.py
+++ server/app.py
@@ -261,6 +261,8 @@ class Offers(Resource):
     def post(self):
         args = offer_parser.parse_args()
         id = args['id']
+        service_ids = ProviderService.query.filter_by(provider_id=id).all()
+
 @app.route('/service', methods=['GET', 'POST'])
 @jwt_required()
 def handle_service_request():
