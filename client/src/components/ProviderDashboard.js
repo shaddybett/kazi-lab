2fc67352a1088f6dabfd7b0e3a962395cba7b8b6
Shaddy <shadybett540@gmail.com>
2024-06-01 22:55:18 +0300

    removed token from the console

--- client/src/components/ProviderDashboard.js
+++ client/src/components/ProviderDashboard.js
@@ -30,7 +30,6 @@ function ProviderDashboard() {
   useEffect(() => {
     const handleEntry = async () => {
       const token = localStorage.getItem("token");
-      console.log(token)
       try {
         const response = await fetch("/dashboard", {
           method: "GET",
