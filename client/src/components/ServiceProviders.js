c407e6b0167773b88437e2fd6a9de4141174a51f
Shaddy <shadybett540@gmail.com>
2024-06-05 21:42:48 +0300

    imported user details popup to service details page

--- client/src/components/ServiceProviders.js
+++ client/src/components/ServiceProviders.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import UserDetailsPopup from "./UserDetailsPopup";
 
 function ServiceProviders() {
   const [providers, setProviders] = useState([]);
