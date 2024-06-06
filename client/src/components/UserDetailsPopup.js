1bdfc6aa49462239c7ba9dc8c3fa53db8faa119e
Shaddy <shadybett540@gmail.com>
2024-06-06 13:15:45 +0300

    fixed indentation

--- client/src/components/UserDetailsPopup.js
+++ client/src/components/UserDetailsPopup.js
@@ -6,38 +6,38 @@ function UserDetailsPopup({ user, onClose }) {
 
   return (
     <div className="flex flex-col items-center">
-        <div className="flex justify-between w-full max-w-4xl mt-20">
+      <div className="flex justify-between w-full max-w-4xl mt-20">
         <Card className="max-w-l">
-        <div className="flex items-center gap-5 mt-10 ml-5">
-          <h2>User Details</h2>
-          <p>
-            <Avatar img={user.image} size="xl" />
-          </p>
-          <p>
-            <strong>First Name:</strong> {user.first_name}
-          </p>
-          <p>
-            <strong>Last Name:</strong> {user.last_name}
-          </p>
-          <p>
-            <strong>Middle Name:</strong> {user.middle_name}
-          </p>
-          <p>
-            <strong>Email:</strong> {user.email}
-          </p>
-          <p>
-            <strong>Role ID:</strong> {user.role_id}
-          </p>
-          <p>
-            <strong>Phone Number:</strong> {user.phone_number}
-          </p>
-          <p>
-            <strong>National ID:</strong> {user.national_id}
-          </p>
-          <button onClick={onClose}>Close</button>
-        </div>
-      </Card>
-        </div>
+          <div className="flex items-center gap-5 mt-10 ml-5">
+            <h2>User Details</h2>
+            <p>
+              <Avatar img={user.image} size="xl" />
+            </p>
+            <p>
+              <strong>First Name:</strong> {user.first_name}
+            </p>
+            <p>
+              <strong>Last Name:</strong> {user.last_name}
+            </p>
+            <p>
+              <strong>Middle Name:</strong> {user.middle_name}
+            </p>
+            <p>
+              <strong>Email:</strong> {user.email}
+            </p>
+            <p>
+              <strong>Role ID:</strong> {user.role_id}
+            </p>
+            <p>
+              <strong>Phone Number:</strong> {user.phone_number}
+            </p>
+            <p>
+              <strong>National ID:</strong> {user.national_id}
+            </p>
+            <button onClick={onClose}>Close</button>
+          </div>
+        </Card>
+      </div>
     </div>
   );
 }
