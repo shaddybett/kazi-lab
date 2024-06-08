464fc2d9d5a676324dcf494b296cf46b3e50c1e4
Shaddy <shadybett540@gmail.com>
2024-06-08 13:10:19 +0300

    imported useeffect

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Label, Checkbox } from "flowbite-react";
 import { Button } from "flowbite-react";
