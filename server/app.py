b0aa86f74f3f08b65e8014cc353ee1749751c048
Shaddy <shadybett540@gmail.com>
2024-06-08 12:41:23 +0300

    made an error return statement

--- server/app.py
+++ server/app.py
@@ -306,7 +306,7 @@ class AddService(Resource):
         if new_service_name:
             existing_service = Service.query.filter(func.lower(Service.service_name) == func.lower(new_service_name)).first()
             exists_for_provider = Service.query.filter_by(provider_id=oid, service_name=new_service_name).first()
-            if exists.service_name == new_service_name:
+            if exists_for_provider:
                 return {'error': f'Service is already registered'}, 401
             if existing_service:
                 return {'error': f'Service "{new_service_name}" already exists, kindly check the list provided'}, 401
