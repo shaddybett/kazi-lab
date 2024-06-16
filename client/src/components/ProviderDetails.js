433cda462c88db9819784c193fc97ac52773a91d
Shaddy <shadybett540@gmail.com>
2024-06-16 21:45:37 +0300

    removed timeout check

--- client/src/components/ProviderDetails.js
+++ client/src/components/ProviderDetails.js
@@ -119,15 +119,6 @@ function ProviderDetails() {
             setError(errorMessage.error);
             setNewService("");
             fetchAllServices();
-            if (
-              errorMessage.error === "At least one service must be provided" &&
-              errorMessage.error === "Service is already registered"
-            ) {
-              const timer = setTimeout(() => {
-                setError("");
-              }, 5000);
-              return () => clearTimeout(timer); // Cleanup the timer on component unmount or error change
-            }
           } else {
             setError(errorMessage.error || "An error occurred");
           }
