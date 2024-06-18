f82e5a6eae55623d9b8933d99e0eee51349272e4
Shaddy <shadybett540@gmail.com>
2024-06-18 22:24:08 +0300

    done

--- client/src/components/ClientDashboard.js
+++ client/src/components/ClientDashboard.js
@@ -1,212 +1,4 @@
 
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { Avatar, Dropdown, Navbar, Button, Card } from "flowbite-react";
-import Swal from "sweetalert2";
-
-function ClientDashboard() {
-  const [data, setData] = useState({});
-  const [error, setError] = useState("");
-  const [services, setServices] = useState([]);
-  const [providers, setProviders] = useState([]);
-  const [searchQuery, setSearchQuery] = useState(""); // Add state for search query
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const token = localStorage.getItem("token");
-        const response = await fetch("/service", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        if (response.ok) {
-          const responseData = await response.json();
-          setServices(responseData.all_services);
-        } else {
-          const errorMessage = await response.json();
-          setError(errorMessage.error || "An error occurred");
-        }
-      } catch (error) {
-        setError("An error occurred. Try again later");
-      }
-    };
-
-    const handleUser = async () => {
-      try {
-        const token = localStorage.getItem("token");
-        const response = await fetch("/dashboard", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        if (response.ok) {
-          const responseData = await response.json();
-          setData(responseData);
-        } else {
-          const errorMessage = await response.json();
-          setError(errorMessage.error || "An error occurred");
-        }
-      } catch (error) {
-        setError("An error occurred. Please try again later");
-      }
-    };
-
-    fetchData();
-    handleUser();
-  }, []);
-
-  const handleProviders = async (service) => {
-    try {
-      const token = localStorage.getItem("token");
-      const response = await fetch(`/provider-ids/${service.id}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (response.ok) {
-        const responseData = await response.json();
-        localStorage.setItem(
-          "providerIds",
-          JSON.stringify(responseData.provider_ids)
-        );
-        const providerIds = responseData.provider_ids.join(",");
-        const userResponse = await fetch(
-          `/provider-details?provider_ids=${providerIds}`,
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        if (userResponse.ok) {
-          const userData = await userResponse.json();
-
-          const providerNames = userData.first_names;
-          setProviders(providerNames);
-          navigate("/providers");
-        } else {
-          const errorMessage = await userResponse.json();
-          setError(errorMessage.error);
-        }
-      } else {
-        const errorMessage = await response.json();
-        setError(
-          errorMessage.error || "An error occurred while fetching provider IDs"
-        );
-      }
-    } catch (error) {
-      setError("An error occurred please try again later");
-    }
-  };
-
-  const handleLogout = async () => {
-    const result = await Swal.fire({
-      title: "Are you sure?",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, Logout!",
-    });
-    if (result.isConfirmed) {
-      localStorage.removeItem("token");
-      navigate("/login");
-    }
-  };
-
-  const handleProfile = () => {
-    navigate("/profile");
-  };
-
-  useEffect(() => {
-    if (error) {
-      const timer = setTimeout(() => {
-        setError('');
-      }, 5000);
-      return () => clearTimeout(timer); // Cleanup the timer on component unmount or error change
-    }
-  }, [error]);
-
-  // Filter services based on search query
-  const filteredServices = services.filter((service) =>
-    service.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
-  return (
-    <div className="p-4">
-      <Navbar fluid rounded className="bg-black">
-        <div className="flex md:order-2">
-          <Dropdown
-            arrowIcon={false}
-            inline
-            label={<Avatar alt="User settings" img={data.image} rounded />}
-          >
-            <Dropdown.Header>
-              <span className="block text-sm">
-                {data.first_name} {data.last_name}
-              </span>
-              <span className="block truncate text-sm font-medium">
-                {data.email}
-              </span>
-            </Dropdown.Header>
-            <Dropdown.Item onClick={handleProfile}>Profile</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
-          </Dropdown>
-          <Navbar.Toggle />
-        </div>
-        <Navbar.Collapse>
-          <Navbar.Link href="/link1" active></Navbar.Link>
-          <Navbar.Link href="/link2"></Navbar.Link>
-          <Navbar.Link href="/link3"></Navbar.Link>
-          <Navbar.Link href="/link4"></Navbar.Link>
-          <Navbar.Link href="/link5"></Navbar.Link>
-        </Navbar.Collapse>
-      </Navbar>
-
-      <div className="mt-4">
-        <input
-          type="text"
-          placeholder="Search services..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          className="p-2 border rounded w-full mb-4 max-w-80 ml-5 "
-        />
-      </div>
-
-      <div className="grid grid-cols-2 sm:grid-cols-4 gap-2">
-        {filteredServices.map((service) => (
-          <div key={service.id} className="mb-4">
-            <Card className="max-w-sm mt-3 ml-5">
-              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {service.name}
-              </h5>
-              <Button onClick={() => handleProviders(service)}>
-                Service Providers
-              </Button>
-            </Card>
-          </div>
-        ))}
-      </div>
-
-      {error && <p className="text-red-500 mt-4">{error}</p>}
-    </div>
-  );
-}
-
-export default ClientDashboard;
-
-
 // import React, { useState, useEffect } from "react";
 // import { useNavigate } from "react-router-dom";
 // import { Avatar, Dropdown, Navbar, Button, Card } from "flowbite-react";
@@ -216,38 +8,26 @@ export default ClientDashboard;
 //   const [data, setData] = useState({});
 //   const [error, setError] = useState("");
 //   const [services, setServices] = useState([]);
-//   const [searchQuery,setSearchQuery] = useState("")
-//   const [clientLocation, setClientLocation] = useState({ latitude: null, longitude: null });
+//   const [providers, setProviders] = useState([]);
+//   const [searchQuery, setSearchQuery] = useState(""); // Add state for search query
 //   const navigate = useNavigate();
 
 //   useEffect(() => {
 //     const fetchData = async () => {
 //       try {
 //         const token = localStorage.getItem("token");
-//         const [serviceResponse, userResponse] = await Promise.all([
-//           fetch("/service", {
-//             method: "GET",
-//             headers: {
-//               "Content-Type": "application/json",
-//               Authorization: `Bearer ${token}`,
-//             },
-//           }),
-//           fetch("/dashboard", {
-//             method: "GET",
-//             headers: {
-//               "Content-Type": "application/json",
-//               Authorization: `Bearer ${token}`,
-//             },
-//           }),
-//         ]);
-
-//         if (serviceResponse.ok && userResponse.ok) {
-//           const serviceData = await serviceResponse.json();
-//           const userData = await userResponse.json();
-//           setServices(serviceData.all_services);
-//           setData(userData);
+//         const response = await fetch("/service", {
+//           method: "GET",
+//           headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${token}`,
+//           },
+//         });
+//         if (response.ok) {
+//           const responseData = await response.json();
+//           setServices(responseData.all_services);
 //         } else {
-//           const errorMessage = await serviceResponse.json();
+//           const errorMessage = await response.json();
 //           setError(errorMessage.error || "An error occurred");
 //         }
 //       } catch (error) {
@@ -255,22 +35,30 @@ export default ClientDashboard;
 //       }
 //     };
 
-//     const fetchClientLocation = () => {
-//       navigator.geolocation.getCurrentPosition(
-//         (position) => {
-//           const location = {
-//             latitude: position.coords.latitude,
-//             longitude: position.coords.longitude,
-//           };
-//           setClientLocation(location);
-//           localStorage.setItem("clientLocation", JSON.stringify(location));
-//         },
-//         (error) => console.error("Error obtaining client location:", error)
-//       );
+//     const handleUser = async () => {
+//       try {
+//         const token = localStorage.getItem("token");
+//         const response = await fetch("/dashboard", {
+//           method: "GET",
+//           headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${token}`,
+//           },
+//         });
+//         if (response.ok) {
+//           const responseData = await response.json();
+//           setData(responseData);
+//         } else {
+//           const errorMessage = await response.json();
+//           setError(errorMessage.error || "An error occurred");
+//         }
+//       } catch (error) {
+//         setError("An error occurred. Please try again later");
+//       }
 //     };
 
 //     fetchData();
-//     fetchClientLocation();
+//     handleUser();
 //   }, []);
 
 //   const handleProviders = async (service) => {
@@ -285,12 +73,36 @@ export default ClientDashboard;
 //       });
 //       if (response.ok) {
 //         const responseData = await response.json();
-//         localStorage.setItem("providerIds", JSON.stringify(responseData.provider_ids));
+//         localStorage.setItem(
+//           "providerIds",
+//           JSON.stringify(responseData.provider_ids)
+//         );
 //         const providerIds = responseData.provider_ids.join(",");
-//         navigate(`/providers?provider_ids=${providerIds}`);
+//         const userResponse = await fetch(
+//           `/provider-details?provider_ids=${providerIds}`,
+//           {
+//             method: "GET",
+//             headers: {
+//               "Content-Type": "application/json",
+//               Authorization: `Bearer ${token}`,
+//             },
+//           }
+//         );
+//         if (userResponse.ok) {
+//           const userData = await userResponse.json();
+
+//           const providerNames = userData.first_names;
+//           setProviders(providerNames);
+//           navigate("/providers");
+//         } else {
+//           const errorMessage = await userResponse.json();
+//           setError(errorMessage.error);
+//         }
 //       } else {
 //         const errorMessage = await response.json();
-//         setError(errorMessage.error || "An error occurred while fetching provider IDs");
+//         setError(
+//           errorMessage.error || "An error occurred while fetching provider IDs"
+//         );
 //       }
 //     } catch (error) {
 //       setError("An error occurred please try again later");
@@ -321,10 +133,11 @@ export default ClientDashboard;
 //       const timer = setTimeout(() => {
 //         setError('');
 //       }, 5000);
-//       return () => clearTimeout(timer);
+//       return () => clearTimeout(timer); // Cleanup the timer on component unmount or error change
 //     }
 //   }, [error]);
 
+//   // Filter services based on search query
 //   const filteredServices = services.filter((service) =>
 //     service.name.toLowerCase().includes(searchQuery.toLowerCase())
 //   );
@@ -367,7 +180,7 @@ export default ClientDashboard;
 //           placeholder="Search services..."
 //           value={searchQuery}
 //           onChange={(e) => setSearchQuery(e.target.value)}
-//           className="p-2 border rounded w-full mb-4 max-w-80 ml-5"
+//           className="p-2 border rounded w-full mb-4 max-w-80 ml-5 "
 //         />
 //       </div>
 
@@ -392,3 +205,190 @@ export default ClientDashboard;
 // }
 
 // export default ClientDashboard;
+
+
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Avatar, Dropdown, Navbar, Button, Card } from "flowbite-react";
+import Swal from "sweetalert2";
+
+function ClientDashboard() {
+  const [data, setData] = useState({});
+  const [error, setError] = useState("");
+  const [services, setServices] = useState([]);
+  const [searchQuery,setSearchQuery] = useState("")
+  const [clientLocation, setClientLocation] = useState({ latitude: null, longitude: null });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const [serviceResponse, userResponse] = await Promise.all([
+          fetch("/service", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }),
+          fetch("/dashboard", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }),
+        ]);
+
+        if (serviceResponse.ok && userResponse.ok) {
+          const serviceData = await serviceResponse.json();
+          const userData = await userResponse.json();
+          setServices(serviceData.all_services);
+          setData(userData);
+        } else {
+          const errorMessage = await serviceResponse.json();
+          setError(errorMessage.error || "An error occurred");
+        }
+      } catch (error) {
+        setError("An error occurred. Try again later");
+      }
+    };
+
+    const fetchClientLocation = () => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const location = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          };
+          setClientLocation(location);
+          localStorage.setItem("clientLocation", JSON.stringify(location));
+        },
+        (error) => console.error("Error obtaining client location:", error)
+      );
+    };
+
+    fetchData();
+    fetchClientLocation();
+  }, []);
+
+  const handleProviders = async (service) => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await fetch(`/provider-ids/${service.id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        const responseData = await response.json();
+        localStorage.setItem("providerIds", JSON.stringify(responseData.provider_ids));
+        const providerIds = responseData.provider_ids.join(",");
+        navigate(`/providers?provider_ids=${providerIds}`);
+      } else {
+        const errorMessage = await response.json();
+        setError(errorMessage.error || "An error occurred while fetching provider IDs");
+      }
+    } catch (error) {
+      setError("An error occurred please try again later");
+    }
+  };
+
+  const handleLogout = async () => {
+    const result = await Swal.fire({
+      title: "Are you sure?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Logout!",
+    });
+    if (result.isConfirmed) {
+      localStorage.removeItem("token");
+      navigate("/login");
+    }
+  };
+
+  const handleProfile = () => {
+    navigate("/profile");
+  };
+
+  useEffect(() => {
+    if (error) {
+      const timer = setTimeout(() => {
+        setError('');
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
+
+  const filteredServices = services.filter((service) =>
+    service.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <div className="p-4">
+      <Navbar fluid rounded className="bg-black">
+        <div className="flex md:order-2">
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={<Avatar alt="User settings" img={data.image} rounded />}
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">
+                {data.first_name} {data.last_name}
+              </span>
+              <span className="block truncate text-sm font-medium">
+                {data.email}
+              </span>
+            </Dropdown.Header>
+            <Dropdown.Item onClick={handleProfile}>Profile</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
+          </Dropdown>
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse>
+          <Navbar.Link href="/link1" active></Navbar.Link>
+          <Navbar.Link href="/link2"></Navbar.Link>
+          <Navbar.Link href="/link3"></Navbar.Link>
+          <Navbar.Link href="/link4"></Navbar.Link>
+          <Navbar.Link href="/link5"></Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <div className="mt-4">
+        <input
+          type="text"
+          placeholder="Search services..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="p-2 border rounded w-full mb-4 max-w-80 ml-5"
+        />
+      </div>
+
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-2">
+        {filteredServices.map((service) => (
+          <div key={service.id} className="mb-4">
+            <Card className="max-w-sm mt-3 ml-5">
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {service.name}
+              </h5>
+              <Button onClick={() => handleProviders(service)}>
+                Service Providers
+              </Button>
+            </Card>
+          </div>
+        ))}
+      </div>
+
+      {error && <p className="text-red-500 mt-4">{error}</p>}
+    </div>
+  );
+}
+
+export default ClientDashboard;
