9652300459261bed81de1da6fbf2e80a82a2aac7
Shaddy <shadybett540@gmail.com>
2024-06-19 11:42:19 +0300

    made tables for county name and id

--- server/models.py
+++ server/models.py
@@ -39,3 +39,5 @@ class Role(db.Model):
     role_name = db.Column(db.String(100), nullable=False)
 class County(db.Model):
     __tablename__='counties'
+    id = db.Column(db.Integer, primary_key=True)
+    county_name = db.Column(db.String(100), nullable=False)
