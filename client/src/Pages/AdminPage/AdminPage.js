5cf7d495955e109dc60eb2aa06ce1ceafa9b0afd
Shaddy <shadybett540@gmail.com>
2024-07-30 18:52:06 +0300

    made tables for clients and providers

--- client/src/Pages/AdminPage/AdminPage.js
+++ client/src/Pages/AdminPage/AdminPage.js
@@ -1,46 +1,121 @@
-import React, {useState} from 'react'
-import { Table } from 'flowbite-react'
+import React, { useState } from "react";
+import { Table } from "flowbite-react";
 
 function AdminPage() {
-    const [providers,setProviders] = useState([]);
-    const [clients,setClients] = useState([]);
-    const [error,setError] = useState(null);
+  const [providers, setProviders] = useState([]);
+  const [clients, setClients] = useState([]);
+  const [error, setError] = useState(null);
 
-    const backendUrl = process.env.REACT_APP_BACKEND_URL
-    const handleUsers = async (e) => {
-        e.preventDefault();
-        const token = localStorage.getItem('token');
+  const backendUrl = process.env.REACT_APP_BACKEND_URL;
+  const handleUsers = async (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem("token");
 
-        try {
-            const response = await fetch(`{backendUrl}/all_users`,{
-                method: 'GET',
-                headers:{
-                    Authorization: `Bearer ${token}`,
-                },
-            });
+    try {
+      const response = await fetch(`{backendUrl}/all_users`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
 
-            if (response.ok){
-                const responseData = await response.json();
-                const fetchedProviders = responseData.filter(user => user.role_id === 2);
-                const fetchedClients = responseData.filter(user => user.role_id === 3);
-                setProviders(fetchedProviders);
-                setClients(fetchedClients);
-            }
-            else {
-                const errorMessage = await response.json();
-                setError(errorMessage.error);
-            }
-        }catch (err) {
-            setError("An unexpected error occurred");
-        }
-    };
+      if (response.ok) {
+        const responseData = await response.json();
+        const fetchedProviders = responseData.filter(
+          (user) => user.role_id === 2
+        );
+        const fetchedClients = responseData.filter(
+          (user) => user.role_id === 3
+        );
+        setProviders(fetchedProviders);
+        setClients(fetchedClients);
+      } else {
+        const errorMessage = await response.json();
+        setError(errorMessage.error);
+      }
+    } catch (err) {
+      setError("An unexpected error occurred");
+    }
+  };
 
   return (
     <div>
-        <button onClick={handleUsers} >All users</button>
-        {error && <p>{error}</p>}
+      <button onClick={handleUsers}>All users</button>
+      {error && <p>{error}</p>}
+      <div className="overflow-x-auto">
+        <h2>Providers</h2>
+        <Table hoverable>
+          <Table.Head>
+            <Table.HeadCell>Name</Table.HeadCell>
+            <Table.HeadCell>Email</Table.HeadCell>
+            <Table.HeadCell>Phone Number</Table.HeadCell>
+            <Table.HeadCell>ID Number</Table.HeadCell>
+            <Table.HeadCell>
+              <span className="sr-only">Edit</span>
+            </Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            {providers.map((user, index) => (
+              <Table.Row
+                key={index}
+                className="bg-white dark:border-gray-700 dark:bg-gray-800"
+              >
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  {user.first_name} {user.last_name}
+                </Table.Cell>
+                <Table.Cell>{user.email}</Table.Cell>
+                <Table.Cell>{user.phone_number}</Table.Cell>
+                <Table.Cell>{user.id}</Table.Cell>
+                <Table.Cell>
+                  <a
+                    href="#"
+                    className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                  >
+                    Edit
+                  </a>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+        <h2>Clients</h2>
+        <Table hoverable>
+          <Table.Head>
+            <Table.HeadCell>Name</Table.HeadCell>
+            <Table.HeadCell>Email</Table.HeadCell>
+            <Table.HeadCell>Phone Number</Table.HeadCell>
+            <Table.HeadCell>ID Number</Table.HeadCell>
+            <Table.HeadCell>
+              <span className="sr-only">Edit</span>
+            </Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            {clients.map((user, index) => (
+              <Table.Row
+                key={index}
+                className="bg-white dark:border-gray-700 dark:bg-gray-800"
+              >
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  {user.first_name} {user.last_name}
+                </Table.Cell>
+                <Table.Cell>{user.email}</Table.Cell>
+                <Table.Cell>{user.phone_number}</Table.Cell>
+                <Table.Cell>{user.id}</Table.Cell>
+                <Table.Cell>
+                  <a
+                    href="#"
+                    className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                  >
+                    Edit
+                  </a>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AdminPage
+export default AdminPage;
