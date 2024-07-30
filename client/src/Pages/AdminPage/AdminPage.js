5a0e826b1c092511a4596fba5e86a98099f23510
Shaddy <shadybett540@gmail.com>
2024-07-30 18:19:26 +0300

    made state variable for error

--- client/src/Pages/AdminPage/AdminPage.js
+++ client/src/Pages/AdminPage/AdminPage.js
@@ -3,7 +3,8 @@ import { Table } from 'flowbite-react'
 
 function AdminPage() {
     const [providers,setProviders] = useState([]);
-    const [clients,setClients] = useState([])
+    const [clients,setClients] = useState([]);
+    const [error,setError] = useState(null);
   return (
     <div>AdminPage</div>
   )
