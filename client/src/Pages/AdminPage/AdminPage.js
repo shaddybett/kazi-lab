2a4b5502979a4306cba103a700f324087ff975d1
Shaddy <shadybett540@gmail.com>
2024-07-31 09:58:28 +0300

    styled title `all users`

--- client/src/Pages/AdminPage/AdminPage.js
+++ client/src/Pages/AdminPage/AdminPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Table } from "flowbite-react";
+import './AdminPage.css'
 
 function AdminPage() {
   const [providers, setProviders] = useState([]);
