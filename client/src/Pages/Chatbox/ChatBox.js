3dd71349e4e33953535d8d864a950c8f0d78a6e1
Shaddy <shadybett540@gmail.com>
2024-08-01 20:52:17 +0300

    done

--- client/src/Pages/Chatbox/ChatBox.js
+++ client/src/Pages/Chatbox/ChatBox.js
@@ -27,20 +27,17 @@ const ChatBox = ({ senderId, receiver, onClose }) => {
 
   const handleSendMessage = async () => {
     try {
-      const response = await fetch(
-        `${backendUrl}/send_message`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            sender_id: senderId,
-            receiver_id: receiver.id,
-            content: newMessage,
-          }),
-        }
-      );
+      const response = await fetch(`${backendUrl}/send_message`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          sender_id: senderId,
+          receiver_id: receiver.id,
+          content: newMessage,
+        }),
+      });
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
@@ -53,26 +50,35 @@ const ChatBox = ({ senderId, receiver, onClose }) => {
 
   return (
     <div className="chat-box">
-      <div className="chat-header">
-        <h3>Chat with {receiver.first_name} {receiver.last_name}</h3>
-        <button onClick={onClose}>Close</button>
-      </div>
-      <div className="chat-body">
-        {messages.map((msg) => (
-          <div key={msg.id} className="chat-message">
-            <p><strong>{msg.sender_id === senderId ? "You" : receiver.first_name}:</strong> {msg.content}</p>
-          </div>
-        ))}
-      </div>
-      <div className="chat-footer">
-        <input
-          type="text"
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-          placeholder="Type a message"
-        />
-        <button onClick={handleSendMessage}>Send</button>
-      </div>
+      <Card>
+        <div className="chat-header">
+          <h3>
+            Chat with {receiver.first_name} {receiver.last_name}
+          </h3>
+          <button onClick={onClose}>Close</button>
+        </div>
+        <div className="chat-body">
+          {messages.map((msg) => (
+            <div key={msg.id} className="chat-message">
+              <p>
+                <strong>
+                  {msg.sender_id === senderId ? "You" : receiver.first_name}:
+                </strong>{" "}
+                {msg.content}
+              </p>
+            </div>
+          ))}
+        </div>
+        <div className="chat-footer">
+          <input
+            type="text"
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+            placeholder="Type a message"
+          />
+          <button onClick={handleSendMessage}>Send</button>
+        </div>
+      </Card>
     </div>
   );
 };
