0a58e0cdfa6f16ce42e7e45df533cf1579f48f42
Shaddy <shadybett540@gmail.com>
2024-08-11 16:36:41 +0300

    done

--- client/src/Pages/Chat/MessageBubble.js
+++ client/src/Pages/Chat/MessageBubble.js
@@ -1,29 +1,61 @@
-import React from "react";
-import classNames from "classnames";
-import "./Chat.css"
+// import React from "react";
+// import classNames from "classnames";
+// import "./Chat.css"
 
-const MessageBubble = ({ message, senderId }) => {
-  // Determine if the message was sent by the admin (senderId) or received
-  const isSentByAdmin = message.sender_id === senderId;
+// const MessageBubble = ({ message, senderId }) => {
+//   // Determine if the message was sent by the admin (senderId) or received
+//   const isSentByAdmin = message.sender_id === senderId;
 
-  // Conditionally apply classes based on who sent the message
-  const bubbleClass = classNames(
-    "p-3 mb-2 rounded-lg max-w-xs break-words",
-    {
-      "bg-blue-500 text-white self-end": isSentByAdmin, // Sent by admin
-      "bg-gray-700 text-white self-start": !isSentByAdmin, // Received
-    },
-    {
-      "ml-auto": isSentByAdmin, // Align to the right
-      "mr-auto": !isSentByAdmin, // Align to the left
-    }
-  );
+//   // Conditionally apply classes based on who sent the message
+//   const bubbleClass = classNames(
+//     "p-3 mb-2 rounded-lg max-w-xs break-words",
+//     {
+//       "bg-blue-500 text-white self-end": isSentByAdmin, // Sent by admin
+//       "bg-gray-700 text-white self-start": !isSentByAdmin, // Received
+//     },
+//     {
+//       "ml-auto": isSentByAdmin, // Align to the right
+//       "mr-auto": !isSentByAdmin, // Align to the left
+//     }
+//   );
 
-  return (
-    <div className={bubbleClass}>
-      <p className="whitespace-pre-wrap">{message.content}</p>
-    </div>
-  );
-};
+//   return (
+//     <div className={bubbleClass}>
+//       <p className="whitespace-pre-wrap">{message.content}</p>
+//     </div>
+//   );
+// };
 
-export default MessageBubble;
+// export default MessageBubble;
+
+
+
+                                            import React from "react";
+                                            import classNames from "classnames";
+                                            import "./Chat.css";
+
+                                            const MessageBubble = ({ message, currentUserId }) => {
+                                              // Determine if the message was sent by the current user
+                                              const isSentByCurrentUser = message.sender_id === currentUserId;
+
+                                              // Conditionally apply classes based on who sent the message
+                                              const bubbleClass = classNames(
+                                                "p-3 mb-2 rounded-lg max-w-xs break-words",
+                                                {
+                                                  "bg-blue-500 text-white self-end": isSentByCurrentUser, // Sent by the current user
+                                                  "bg-gray-700 text-white self-start": !isSentByCurrentUser, // Received
+                                                },
+                                                {
+                                                  "ml-auto": isSentByCurrentUser, // Align to the right
+                                                  "mr-auto": !isSentByCurrentUser, // Align to the left
+                                                }
+                                              );
+
+                                              return (
+                                                <div className={bubbleClass}>
+                                                  <p className="whitespace-pre-wrap">{message.content}</p>
+                                                </div>
+                                              );
+                                            };
+
+                                            export default MessageBubble;
