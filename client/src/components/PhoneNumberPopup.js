a37b17307333193e1a7d07f834648d4bf17f1377
Shaddy <shadybett540@gmail.com>
2024-08-13 02:53:53 +0300

    done

--- client/src/components/PhoneNumberPopup.js
+++ client/src/components/PhoneNumberPopup.js
@@ -99,30 +99,35 @@ function PhoneNumberPopup({ phoneNumber, onClose }) {
   const onCloseChat =()=>{
     setChatUser(null)
   }
-  const admins = [5,4]
+  const admins = [5, 4];
   const handleSendMessage = async (messageContent) => {
     try {
-      const response = await fetch(`${backendUrl}/send_message`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          sender_id: currentUserId,
-          receiver_id: admins,
-          content: messageContent,
-        }),
+      const sendMessages = admins.map(async (adminId) => {
+        const response = await fetch(`${backendUrl}/send_message`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            sender_id: currentUserId,
+            receiver_id: adminId, // Individual admin ID
+            content: messageContent,
+          }),
+        });
+  
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
       });
-
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
+  
+      await Promise.all(sendMessages);
+  
     } catch (error) {
       console.error("Error sending message:", error);
       setError("Error sending message");
     }
   };
+  
   return (
     <div
       className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
