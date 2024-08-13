8c47282ab4c89a72f102c4cdf1857a6c32ef87ed
Shaddy <shadybett540@gmail.com>
2024-08-13 12:52:14 +0300

    made blocked user popup

---
+++ client/src/Pages/LoginPage/BlockedUser.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button, Card, Checkbox, Label, TextInput } from "flowbite-react";
+
+function BlockedUser() {
+  return (
+    <div>BlockedUser</div>
+  )
+}
+
+export default BlockedUser
