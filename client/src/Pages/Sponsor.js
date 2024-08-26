07699fc749963f7e4a0f6c3592d19e032d905235
Shaddy <shadybett540@gmail.com>
2024-08-26 23:23:32 +0300

    done

--- client/src/Pages/Sponsor.js
+++ client/src/Pages/Sponsor.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import PhoneNumberPopup from "../components/PhoneNumberPopup";
+import PayPopup from "./PayPopup";
 
 const stripePromise = loadStripe(
   "pk_live_51PpWVz2LNaBLa9OHujPAFFVNHonHKiydkK5BTWeIIDfSfsTX6nOOXlfYZ57dRV6hzOVNwQW4Q7V9SWZAq6DNi1AG00me1GFA0D"
@@ -16,7 +16,7 @@ function Sponsor() {
       <button onClick={handlePay}>Pay</button>
       {showPopup && (
         <Elements stripe={stripePromise}>
-          <PhoneNumberPopup onClose={() => setShowPopup(false)} />
+          <PayPopup onClose={() => setShowPopup(false)} />
         </Elements>
       )}
     </div>
