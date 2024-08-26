6cf5a7e9efe9f26643866765733c2daacc95700a
Shaddy <shadybett540@gmail.com>
2024-08-26 22:59:51 +0300

    made variable show popup

--- client/src/Pages/Sponsor.js
+++ client/src/Pages/Sponsor.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from "@stripe/stripe-js";
 import PhoneNumberPopup from '../components/PhoneNumberPopup'
 
 const stripePromise = loadStripe("pk_live_51PpWVz2LNaBLa9OHujPAFFVNHonHKiydkK5BTWeIIDfSfsTX6nOOXlfYZ57dRV6hzOVNwQW4Q7V9SWZAq6DNi1AG00me1GFA0D");
 function Sponsor() {
+    const [showPopup, setShowPopup] = useState(false)
   return (
-    <div>Sponsor</div>
+    <div></div>
   )
 }
 
