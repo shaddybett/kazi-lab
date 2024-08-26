e2ae920b04cf28d6a08dc85b541c9d72eb2bc54c
Shaddy <shadybett540@gmail.com>
2024-08-26 23:07:21 +0300

    done

--- client/src/Pages/Sponsor.js
+++ client/src/Pages/Sponsor.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react'
-import { Elements } from '@stripe/react-stripe-js'
+import React, { useState } from "react";
+import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import PhoneNumberPopup from '../components/PhoneNumberPopup'
+import PhoneNumberPopup from "../components/PhoneNumberPopup";
 
-const stripePromise = loadStripe("pk_live_51PpWVz2LNaBLa9OHujPAFFVNHonHKiydkK5BTWeIIDfSfsTX6nOOXlfYZ57dRV6hzOVNwQW4Q7V9SWZAq6DNi1AG00me1GFA0D");
+const stripePromise = loadStripe(
+  "pk_live_51PpWVz2LNaBLa9OHujPAFFVNHonHKiydkK5BTWeIIDfSfsTX6nOOXlfYZ57dRV6hzOVNwQW4Q7V9SWZAq6DNi1AG00me1GFA0D"
+);
 function Sponsor() {
-    const [showPopup, setShowPopup] = useState(false)
-    const handlePay = ()=>{
-        setShowPopup(true)
-    }
+  const [showPopup, setShowPopup] = useState(false);
+  const handlePay = () => {
+    setShowPopup(true);
+  };
   return (
-    <div></div>
-  )
+    <div>
+      <button onClick={handlePay}>Pay</button>
+      {showPopup && (
+        <Elements stripe={stripePromise}>
+          <PhoneNumberPopup onClose={() => setShowPopup(false)} />
+        </Elements>
+      )}
+    </div>
+  );
 }
 
-export default Sponsor
+export default Sponsor;
