955fa2b2e7f0b44b03e64c85f7b70f6c071212a1
Shaddy <shadybett540@gmail.com>
2024-08-26 23:01:00 +0300

    made function handlepay

--- client/src/Pages/Sponsor.js
+++ client/src/Pages/Sponsor.js
@@ -6,6 +6,9 @@ import PhoneNumberPopup from '../components/PhoneNumberPopup'
 const stripePromise = loadStripe("pk_live_51PpWVz2LNaBLa9OHujPAFFVNHonHKiydkK5BTWeIIDfSfsTX6nOOXlfYZ57dRV6hzOVNwQW4Q7V9SWZAq6DNi1AG00me1GFA0D");
 function Sponsor() {
     const [showPopup, setShowPopup] = useState(false)
+    const handlePay = ()=>{
+        setShowPopup(true)
+    }
   return (
     <div></div>
   )
