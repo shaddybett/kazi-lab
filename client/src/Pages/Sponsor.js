6728f962d28195cb1de6e6b9752ca2edc8391a23
Shaddy <shadybett540@gmail.com>
2024-08-26 22:57:30 +0300

    made variable stripe promise with publishable key

--- client/src/Pages/Sponsor.js
+++ client/src/Pages/Sponsor.js
@@ -3,6 +3,7 @@ import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from "@stripe/stripe-js";
 import PhoneNumberPopup from '../components/PhoneNumberPopup'
 
+const stripePromise = loadStripe("pk_live_51PpWVz2LNaBLa9OHujPAFFVNHonHKiydkK5BTWeIIDfSfsTX6nOOXlfYZ57dRV6hzOVNwQW4Q7V9SWZAq6DNi1AG00me1GFA0D");
 function Sponsor() {
   return (
     <div>Sponsor</div>
