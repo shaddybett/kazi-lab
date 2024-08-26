db2fec0ee3dc227c6b1670a206a5db278a2371d8
Shaddy <shadybett540@gmail.com>
2024-08-26 23:35:47 +0300

    made stae variable for amount

--- client/src/Pages/Sponsored.js
+++ client/src/Pages/Sponsored.js
@@ -3,9 +3,18 @@ import React, { useState } from 'react'
 function Sponsored() {
   const [bankCode,setBankCode] = useState(null)
   const [bankAccount,setBankAccount] = useState(null)
+  const [amount,setAmount] = useState(null)
+
+  const handleBankCode = ()=>{
+    setBankCode()
+  }
+  const handleBankAccount = ()=>{
+    setBankAccount()
+  }
+  const 
   return (
     <div>
-
+      <input type='text'  />
     </div>
   )
 }
