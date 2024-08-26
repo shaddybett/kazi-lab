ca5a19c4c8a423221e5fc270a81981056b829821
Shaddy <shadybett540@gmail.com>
2024-08-26 22:56:42 +0300

    imported load stripe

--- client/src/Pages/Sponsor.js
+++ client/src/Pages/Sponsor.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from "@stripe/stripe-js";
 import PhoneNumberPopup from '../components/PhoneNumberPopup'
 
 function Sponsor() {
