fff6bd27ea7730a1935dfdbbc19d358de65aaab8
Shaddy <shadybett540@gmail.com>
2024-08-27 17:51:08 +0300

    made function  validate input to check for bank code and amount formats

--- client/src/Pages/Sponsored.js
+++ client/src/Pages/Sponsored.js
@@ -6,6 +6,18 @@ function Sponsored() {
   const [amount,setAmount] = useState('')
   const [message,setMessage] = useState('')
   const [error,setError] = useState('')
+
+  const validateInput = ()=>{
+    if (!bankCode.match(/^\d{6}$/)) {
+      setError('Invalid bank code');
+      return false;
+    }
+    if (!bankAccount.match(/^\d{12}$/)) {
+      setError('Invalid bank account number');
+      return false;
+    }
+    return true
+  };
   
   return (
     <div>Sponsored</div>
