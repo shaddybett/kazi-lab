ef776a19218cfc7fc0f29da309a0907dfa69b58a
Shaddy <shadybett540@gmail.com>
2024-08-27 17:33:10 +0300

    done

--- client/src/Pages/Sponsored.js
+++ client/src/Pages/Sponsored.js
@@ -4,6 +4,8 @@ function Sponsored() {
   const [bankCode,setBankCode] = useState(null)
   const [bankAccount,setBankAccount] = useState(null)
   const [amount,setAmount] = useState(null)
+  const [message,setMessage] = useState([])
+  const [error,setError] = useState([])
 
   const handleBankCode = ()=>{
     setBankCode()
@@ -14,9 +16,35 @@ function Sponsored() {
   const handleAmount = ()=>{
     setAmount()
   }
+  const handleUserDetails = async(e)=>{
+    e.preventDefault()
+    try{
+      const response = await fetch('/needy',{
+        method: 'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify({amount,bankCode,bankAccount})
+      })
+      if (response.ok){
+        const responseData= await response.json()
+        setMessage(responseData.message)
+      }
+      else{
+        const errorMessage = await response.json()
+        setError(errorMessage.error)
+      }
+    }catch (error){
+      setError('An error occurred please try again later')
+    }
+  }
   return (
-    <div>
-      <input type='text'  />
+    <div onSubmit={handleUserDetails} >
+      <input type='text' value={bankCode} />
+      <input type='text' value={bankAccount} />
+      <input type='text' value={amount} />
+      <button type='submit' >submit</button>
+     
     </div>
   )
 }
