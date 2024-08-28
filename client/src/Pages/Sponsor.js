730a26d00d1309f9a88b789041de25ec10143a5d
Shaddy <shadybett540@gmail.com>
2024-08-28 13:05:24 +0300

    made function handle pay

--- client/src/Pages/Sponsor.js
+++ client/src/Pages/Sponsor.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import PayPopup from "./PayPopup";
@@ -8,17 +8,53 @@ const stripePromise = loadStripe(
 );
 function Sponsor() {
   const [showPopup, setShowPopup] = useState(false);
-  const handlePay = () => {
-    setShowPopup(true);
+  const [students,setStudents] = useState([]);
+  const [selectedStudent,setSelectedStudent] = useState(null);
+  const [message,setMessage] = useState('');
+  const [error,setError] = useState('');
+
+  const handleNeedyStudents = async()=>{
+    try{
+      const response = await fetch('/fetch_needy',{
+        method:'GET',
+        headers:{
+          "Content-Type":"application/json",
+        },
+      });
+      if (response.ok){
+        const responseData = await response.json();
+        setStudents(responseData.users)
+        setMessage(responseData.message)
+      }
+      else{
+        const errorMessage = await response.json();
+        setError(errorMessage.error);
+      }
+    }catch (error){
+      setError('An error occurred, please try again later');
+    }
   };
+
+  useEffect(()=>{
+    handleNeedyStudents();
+  },[]);
+
+  const handlePay = (student)=>{
+    setSelectedStudent(student);
+    setShowPopup(true);
+  }
+
   return (
     <div>
-      <button onClick={handlePay}>Pay</button>
+      {students && <div id="students.id"><button onClick={handlePay} >{students}</button></div>}
+
       {showPopup && (
         <Elements stripe={stripePromise}>
-          <PayPopup onClose={() => setShowPopup(false)} />
+          <PayPopup onClose={() => setShowPopup(false)} bankCode={students.bank_code} bankAccount={students.bank_account} amount={students.amount} stripe_id={students.stripe_id} />
         </Elements>
       )}
+      {error && <p style={{color:'red'}} >{error}</p>}
+      {message && <p style={{color:'green'}} >{message}</p>}
     </div>
   );
 }
