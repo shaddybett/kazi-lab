de08caf78bfbeb240009d9e1b0abcef22ecbec15
Shaddy <shadybett540@gmail.com>
2024-09-02 11:18:37 +0300

    added a button to handle onclose

--- client/src/Pages/AdminPage/BlockedUsers.js
+++ client/src/Pages/AdminPage/BlockedUsers.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-function BlockedUsers({blocked}) {
+function BlockedUsers({blocked, onClose}) {
   return (
     <div>
+        <button onClick={onClose} >Close</button>
         {blocked.length > 0 ? <div key={blocked.id} >
         {blocked.first_name} {blocked.last_name} {blocked.reason}
         </div> : <p>No blocked users to display</p>}
