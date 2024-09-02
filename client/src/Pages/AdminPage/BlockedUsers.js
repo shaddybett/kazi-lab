36fef723d35fd9f7b606fc6039f02d6a90e1de24
Shaddy <shadybett540@gmail.com>
2024-09-02 11:24:00 +0300

    done

--- client/src/Pages/AdminPage/BlockedUsers.js
+++ client/src/Pages/AdminPage/BlockedUsers.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-function BlockedUsers({blocked, onClose}) {
+function BlockedUsers({ blocked, onClose }) {
   return (
     <div>
-        <button onClick={onClose} >Close</button>
-        {blocked.length > 0 ? <div key={blocked.id} >
-        {blocked.first_name} {blocked.last_name} {blocked.reason}
-        </div> : <p>No blocked users to display</p>}
+      <button onClick={onClose}>Close</button>
+      {blocked.length > 0 ? (
+        blocked.map((user) => (
+          <div key={user.id}>
+            {user.first_name} {user.last_name} - {user.reason}
+          </div>
+        ))
+      ) : (
+        <p>No blocked users to display</p>
+      )}
     </div>
-  )
+  );
 }
 
-export default BlockedUsers
+export default BlockedUsers;
