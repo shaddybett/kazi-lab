98c1da3e2741c6c3c61dc5d51dd9421cd9aced23
Shaddy <shadybett540@gmail.com>
2024-11-04 22:16:18 +0300

    done

--- client/src/components/ProviderDashboard.js
+++ client/src/components/ProviderDashboard.js
@@ -7,7 +7,7 @@ import {
   HiShoppingBag,
   HiUser,
   HiArrowSmLeft,
-  HiMenu,
+  HiMenu
 } from "react-icons/hi";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
