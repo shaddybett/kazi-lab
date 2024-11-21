eb06b408abc04e7024776e7a4baaa82aa60a9ff6
Shaddy <shadybett540@gmail.com>
2024-11-21 11:36:18 +0300

    altered to send whole url to the backend

--- client/src/components/Uploads.js
+++ client/src/components/Uploads.js
@@ -113,7 +113,7 @@ function Uploads({ minimized }) {
     if (result.isConfirmed) {
       try {
         const token = localStorage.getItem("token");
-        const fileName = fileUrl.split("/").pop();
+        const fileName = fileUrl
         console.log('filefilefiletype',fileType)
         console.log('fileName',fileName)
         const deleteResponse = await fetch(
