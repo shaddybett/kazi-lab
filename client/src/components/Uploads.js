46620b9cfc16014426a794baea5d4215aa86f958
Shaddy <shadybett540@gmail.com>
2024-11-23 19:19:22 +0300

    reverted changes

--- client/src/components/Uploads.js
+++ client/src/components/Uploads.js
@@ -114,7 +114,7 @@ function Uploads({ minimized }) {
     if (result.isConfirmed) {
       try {
         const token = localStorage.getItem("token");
-        const fileName = fileUrl
+        const fileName = fileUrl.split("/").pop();
         console.log('filefilefiletype',fileType)
         console.log('fileName',fileName)
         const deleteResponse = await fetch(
