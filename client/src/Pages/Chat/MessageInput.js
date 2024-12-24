aac2a91818cd76f8adab46fe98fde002d3a437d7
Shaddy <shadybett540@gmail.com>
2024-12-24 22:49:03 +0300

    done

--- client/src/Pages/Chat/MessageInput.js
+++ client/src/Pages/Chat/MessageInput.js
@@ -9,7 +9,6 @@ const MessageInput = ({ sendMessage }) => {
   const mediaRecorderRef = useRef(null);
   const chunksRef = useRef([]);
 
-  
   useEffect(() => {
     function handleClickOutside(event) {
       if (showEmojiOptions && !event.target.closest(".emoji-picker")) {
